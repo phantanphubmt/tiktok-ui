32d2b0c7199274d310d0eecd90b4b490ec8b9869
phu.phan <phu.phan@hazza.com>
2022-12-02 11:10:14 +0700

    update code tiktok

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -56,6 +56,13 @@ function Search() {
         setShowResult(false);
     };
 
+    const handleChange = (e) => {
+        const searchValue = e.target.value;
+        if (!searchValue.startsWith(' ')) {
+            setSearchValue(searchValue);
+        }
+    };
+
     return (
         <HeadlessTippy
             interactive
@@ -78,7 +85,7 @@ function Search() {
                     value={searchValue}
                     placeholder="Search accounts and videos"
                     spellCheck={false}
-                    onChange={(e) => setSearchValue(e.target.value)}
+                    onChange={handleChange}
                     onFocus={() => setShowResult(true)}
                 />
                 {!!searchValue && !loading && (
